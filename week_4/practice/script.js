0ca615395dab33cea64d296bca0b4b5d9297e524
Shoaib Ali <sind98449@gmail.com>
2024-02-13 16:59:26 +0500

    Practice week 4

---
+++ week_4/practice/script.js
@@ -0,0 +1,27 @@
+console.log("Hi I am Shooaib")
+console.error("I am an error")
+console.warn("I am a warning")
+
+console.log("Printing forloop")
+
+for (let i = 0; i < 5; i++) {
+  console.log("Iteration " + (i + 1));
+}
+
+alert("Do not visit this page")
+console.log(typeof({}))
+
+const myObj = {
+  name:"Shoaib",
+  cms: "021-21-0045",
+ address:{
+    city: "Tando Adam",
+    country: "Pakistan"
+  }
+}
+
+console.log(myObj)
+
+for (const i in myObj){
+  console.log(myObj[i])
+}
